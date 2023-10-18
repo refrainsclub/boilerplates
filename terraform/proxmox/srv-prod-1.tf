daf934807c02e8fc50569f38649d31cfc94a60de
James Blair <james@blair.nz>
2023-10-19 12:08:49 +1300

    update ip address

--- terraform/proxmox/srv-prod-1.tf
+++ terraform/proxmox/srv-prod-1.tf
@@ -37,7 +37,7 @@ resource "proxmox_vm_qemu" "srv-prod-1" {
     os_type = "cloud-init"
 
     # (Optional) IP Address and Gateway
-    # ipconfig0 = "ip=0.0.0.0/0,gw=0.0.0.0"
+    ipconfig0 = "ip=10.0.0.50/0,gw=10.0.0.1"
     
     # (Optional) Default User
     ciuser = "james"
